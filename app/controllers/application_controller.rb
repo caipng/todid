ce834a6a3af68d4a8d21e1da718283a236a81379
caipng <caipngg@gmail.com>
2021-01-05 01:32:30 +0800

    added authorize_request in app controller and jwt class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+    before_action :authorize_request
+
+    def authorize_request
+        # { Authorization: 'Bearer <token>' }
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = JsonWebToken.decode(header)
+            @user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+            render json: { error: e.messae }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            # catch invalid/expired tokens
+            render json: { error: e.message }, status: :unauthorized
+        end
+    end
 end
